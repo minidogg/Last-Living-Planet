3b74f6b362a0ce0dc07b319e43cf58587be5624b
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-07-31 16:52:27 -0500

    fixed buildings index.js

--- src/js/buildings/index.js
+++ src/js/buildings/index.js
@@ -1,26 +1,16 @@
-import fs from 'fs';
-import path from 'path';
-
-const buildingsDir = path.resolve(__dirname);
-
 /**
  * Scans the buildings directory and dynamically imports building modules.
  * @returns {Promise<Object[]>} Array of building info objects.
  */
 async function getAllBuildings() {
-    const buildings = [];
-    
-    const directories = fs.readdirSync(buildingsDir, { withFileTypes: true })
-        .filter(dirent => dirent.isDirectory() && dirent.name !== 'index.js')
-        .map(dirent => dirent.name);
-    
-    for (const dir of directories) {
-        const buildingPath = path.join(buildingsDir, dir, 'index.js');
-        if (fs.existsSync(buildingPath)) {
-            const { info } = await import(buildingPath);
-            buildings.push(info);
-        }
+    const buildings = [
+        "house",
+    ].map(folderName=>`./${folderName}/index.js`)
+
+    for(let i = 0;i<buildings.length;i++){
+        buildings[i] = await import(buildings[i])
     }
+    
 
     return buildings;
 }
@@ -31,4 +21,5 @@ async function getAllBuildings() {
     console.log(buildings);
 })();
 
+export const buildings = getAllBuildings()
 export { getAllBuildings };
