755e834b52144d22351c7778d74de9a33ffe8a04
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-08-01 12:08:01 -0500

    image added to resources

--- src/js/resources.js
+++ src/js/resources.js
@@ -2,11 +2,13 @@ import { engine } from "./_main.js";
 
 export const resources = {}
 class Resource{
-    constructor({id, name=id, description=name, startValue: startingValue=0}){
+    constructor({id, name=id, description=name, startValue: startingValue=0, image="tiles/placeholder1.png"}){
         this.id = id;
         this.name = name;
         this.description = description;
         this.startingValue = startingValue;
+        this.image = new Image()
+        this.image.src = "/assets/img/"+image
     }
 }
 
