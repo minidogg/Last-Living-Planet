751f9d24618bb980c3b27367499e2296e1cfaa92
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-09-29 19:30:20 -0500

    spidididid

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,7 @@
 import { engine } from "./_main.js"
 
 let backgroundImg = undefined;
+let saturnImg = undefined;
 
 
 /** 
@@ -8,7 +9,15 @@ let backgroundImg = undefined;
  * @param {CanvasRenderingContext2D} param0.ctx 
  */
 export function BackgroundRender({ctx}){
+    if(saturnImg==undefined)saturnImg = engine.LoadSprite("/assets/img/saturn.png", "saturnImg")
     if(backgroundImg==undefined)backgroundImg = engine.LoadSprite("/assets/img/space.png", "backgroundPlaceholder")
     ctx.drawImage(backgroundImg, 0, -100, backgroundImg.width*1.8, backgroundImg.height*1.8)
     ctx.drawImage(backgroundImg, backgroundImg.width*1.8, -100, backgroundImg.width*1.8, backgroundImg.height*1.8)
+
+    let rot = 50 * Math.PI / 180
+    // ctx.rotate(rot)
+    ctx.globalAlpha = 0.8;
+    ctx.drawImage(saturnImg, 0+engine.camX, 0+engine.camY, saturnImg.width*engine.camZoom, saturnImg.height*engine.camZoom)
+    ctx.globalAlpha = 1;
+    // ctx.rotate(-rot)
 }
